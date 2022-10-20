b967dd14b3e6f2b263041a15dc1c5f4e636d2dc0
Hyperlast <72891329+Hyperlast@users.noreply.github.com>
2022-10-20 16:22:25 +0300

    Project reallocation

--- Playground/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(StockExchange.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
-        System.out.println(StockExchange.maxProfit(new int[]{1, 2, 3, 4, 5}));
-        System.out.println(StockExchange.maxProfit(new int[]{7, 6, 5, 4, 3, 2, 1}));
-    }
-}
